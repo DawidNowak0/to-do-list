13ae82afd3ed8bc07f4fd220965efa6ec3cc99c5
Dawid Nowak <106425610+DawidNowak0@users.noreply.github.com>
2023-04-03 16:30:50 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 function newToDo(){
     const main = document.querySelector('main');
+    const btn = document.querySelector('.newToDo');
 
-    const creating = '<form action="index.php" method="post"><textarea type="text" name="title" class="title">To Do Title</textarea><select name="" id=""></select><textarea type="text" name="content" class="content">To Do Details</textarea><button class="send"></button></form>';
+    const creating = '<form action="index.php" method="post"><textarea type="text" name="title" class="title">To Do Title</textarea><select name="todo" value="To Do">To Do</select><select name="inProgress" value="Inprogress">In progress</select><select name="done" value="Done">Done</select><textarea type="text" name="content" class="content">To Do Details</textarea><button class="send"></button></form>';
 
     main.innerHTML = creating;
+    btn.innerHTML = "";
 
 };
 
