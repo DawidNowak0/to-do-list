82830582d751b7b4d26bea2b857cb7efe67f6912
Dawid Nowak <106425610+DawidNowak0@users.noreply.github.com>
2023-04-06 13:43:56 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -2,10 +2,9 @@ function newToDo(){
     const main = document.querySelector('main');
     const btn = document.querySelector('.newToDo');
 
-    const creating = '<form action="index.php" method="post"><textarea type="text" name="title" class="title">To Do Title</textarea><select ><option value="toDo" name="toDo">To Do</option><option value="inProgress" name="inProgress">In progress</option><option value="Done" name="done">Done</option></select><textarea type="text" name="content" class="content">To Do Details</textarea><button class="send">Send</button></form>';
+    const creating = '<form method="post"><textarea type="text" name="title" class="title">To Do Title</textarea><select name="select"><option value="to_do">To Do</option><option value="in_progress">In progress</option><option value="done">Done</option></select><textarea type="text" name="content" class="content">To Do Details</textarea><button class="send" name="send">Send</button></form>';
 
     main.innerHTML = creating;
     btn.innerHTML = "";
 
 };
-
--- script.php
+++ script.php
@@ -22,4 +22,41 @@
         echo "</div>";
         $conn -> close();
     };
+
+    function addTask($table){
+        $title = $_POST['content'];
+        $content = $_POST['title'];
+
+        $conn = mysqli_connect("localhost", "root", "", "to_do_list");
+        $SQL = $conn->query("INSERT INTO `$table`(`title`, `details`) VALUES ('$title','$content')");
+
+        if($SQL === TRUE){
+            echo "Dodana do listy";
+
+        }
+        else{
+            echo "Error". $SQL . "<br>". $conn->error;
+
+        };
+
+        $conn -> close();
+    };
+
+    if(isset($_POST['send'])){
+        if(isset($_POST['select'])){
+            $select = $_POST['select'];
+            switch ($select){
+                case 'to_do':
+                    $table = 'to_do';
+                    break;
+                case 'in_progress':
+                    $table = 'in_progress';
+                    break;
+                case 'done':
+                    $table = 'done';
+                    break;
+            };
+            addTask($table);
+        };
+    };
 ?>
