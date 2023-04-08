bff7cc7f0151f3a2abc6b1959455ff153d91e051
Dawid Nowak <106425610+DawidNowak0@users.noreply.github.com>
2023-04-08 17:45:36 +0200

    Add files via upload
    Move btn work

--- script.php
+++ script.php
@@ -3,19 +3,40 @@
         $conn = mysqli_connect("localhost", "root", "", "to_do_list");
         $SQL = $conn->query("SELECT title, details, id FROM $table");
 
+        switch ($table){
+            case 'to_do':
+                $h2 = 'To Do';
+                $btn = ['in_progress', 'done'];
+                $btnName = ['In progres', 'Done'];
+                break;
+            case 'in_progress':
+                $h2 = 'In progress';
+                $btn = ['to_do', 'done'];
+                $btnName = ['To Do', 'Done'];
+                break;
+            case 'done':
+                $h2 = 'Done';
+                $btn = ['to_do', 'in_progress'];
+                $btnName = ['To Do', 'In progres'];
+                break;
+        };
+
         echo "<div class='scroll'>";
+        echo "<h2>$h2</h2>";
 
         while($row = mysqli_fetch_row($SQL)){
             echo "<form method='post'>
                     <section>
-                        <div>
+                        <div class='toDo'>
                             <h2>$row[0]</h2>
                         </div>
                         <div class='content'>
                             <p>$row[1]</p>
                         </div>
-                        <button type='submit' name='move'>Move</button>
-                        <button type='submit' name='delete' value='DELETE FROM `$table` WHERE `id` = $row[2]'>Delete</button>
+                        <h3>Move to</h3>
+                        <button type='submit' class='moveBtn' name='move' value='$btn[0] $table $row[2]'>$btnName[0]</button>
+                        <button type='submit' class='moveBtn' name='move' value='$btn[1] $table $row[2]'>$btnName[1]</button>
+                        <button type='submit' class='delete' name='delete' value='DELETE FROM `$table` WHERE `id` = $row[2]'>Delete</button>
                     </section>
                 </form>";
         };
@@ -29,7 +50,7 @@
         $content = $_POST['title'];
 
         $conn = mysqli_connect("localhost", "root", "", "to_do_list");
-        $SQL = $conn->query("INSERT INTO `$table`(`title`, `details`) VALUES ('$title','$content')");
+        $SQL = $conn->query("INSERT INTO `$table`(`title`, `details`) VALUES ('$content','$title')");
 
         if($SQL === TRUE){
             header("refresh: 1; url=index.php");
@@ -42,15 +63,23 @@
     };
 
     function deleteTask(){
-        $id = $_POST['delete'];
+        $SQL = $_POST['delete'];
 
         $conn = mysqli_connect("localhost", "root", "", "to_do_list");
-        $SQL = $conn->query("$id");
+        $del = $conn->query("$SQL");
+
+        $conn -> close();
     };
 
     function moveTask(){
-        header("refresh: 1; url=index.php");
+        $SQL = $_POST['move'];
+        $SQL = explode(" ", $SQL);
 
+        $conn = mysqli_connect("localhost", "root", "", "to_do_list");
+        $move = $conn->query("INSERT INTO `$SQL[0]`(`title`, `details`) SELECT `title`, `details` FROM `$SQL[1]` WHERE `id` = $SQL[2]");
+        $del = $conn->Query("DELETE FROM `$SQL[1]` WHERE `id` = $SQL[2]");
+
+        $conn -> close();
     };
 
     if(isset($_POST['send'])){
