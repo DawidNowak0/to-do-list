9d5c5c0396b61e4df4d21622e0d30d9d1082083b
Dawid Nowak <dawid.nowak040@gmail.com>
2023-04-11 15:12:56 +0200

    delete useless comment

--- index.php
+++ index.php
@@ -29,18 +29,3 @@
     </main>
 </body>
 </html>
-
-<!--
-
-<form method="post">
-    <textarea type="text" name="title" class="title">To Do Title</textarea>
-    <select name="select">
-        <option value="to_do">To Do</option>
-        <option value="in_progress">In progress</option>
-        <option value="done">Done</option>
-    </select>
-    <textarea type="text" name="content" class="content">To Do Details</textarea>
-    <button class="send" name="send">Send</button>
-</form>
-
--->
